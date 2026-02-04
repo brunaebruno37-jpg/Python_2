f24c0ee1852962cb0163bc405027a2b2819c5cff
Bruno_MOREIRA <brunaebruno37@gmail.com>
2026-02-04 20:55:56 -0300

    teste

--- file_reader.py
+++ file_reader.py
@@ -3,6 +3,6 @@
     print(content)'''
 
 
-with open('C:\Users\bruna\Desktop\caixa 4\pi_digits.txt') as file_object:
+with open('imagens\pi_digits.txt') as file_object:
     conteudo = file_object.read()
     print(conteudo)
