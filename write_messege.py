81e5b62dbdee4a7ae0b6d44cec9918e98c867672
Bruno Moreira <brunaebruno37@gmail.com>
2026-02-06 18:49:13 -0300

    Atualizar o write_messege.py

--- write_messege.py
+++ write_messege.py
@@ -7,3 +7,5 @@ with open(file_name, 'w') as file_object:
 with open(file_name, 'a') as file_object:
     file_object.write('I also love finding meaning in large datasets.\n')
     file_object.write('I love creating apps that can run in a browser.\n')
+
+print('Eu amo minha princesa')
