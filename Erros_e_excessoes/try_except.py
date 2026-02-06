b7d258b72a40c230f5adb02a3d78d8268adda050
Bruno_MOREIRA <brunaebruno37@gmail.com>
2026-02-05 23:16:17 -0300

    ok

---
+++ Erros_e_excessoes/try_except.py
@@ -0,0 +1,13 @@
+while True:
+    first_number = input("First number: ")
+    if first_number == 'q':
+        break
+     
+    second_number = input("Second number: ")
+    try:
+ 
+        answer = int(first_number) / int(second_number)
+    except ZeroDivisionError:
+        print("You can't divide by 0!")
+    else:
+        print(answer)
