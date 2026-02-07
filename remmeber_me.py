04a5cea89e6130f3a929abad281056716a53cc8f
Bruno_MOREIRA <brunaebruno37@gmail.com>
2026-02-06 21:17:45 -0300

    alterações
    Usar arquivo em que já foi preenchido por outro programa

--- remmeber_me.py
+++ remmeber_me.py
@@ -1,10 +1,19 @@
 import json
 
-username = input('what is your name? ')
 
-filename = 'username.json'
 
-with open(filename, 'w') as f:
-    json.dump(username, f)
-    print(f'We"ll remember you when you come back, {username}!')
+def greet_user():
+    filename = 'username.json'
+    try:
+        with open(filename, 'r') as f:
+            username = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        username = input('whats is your name: ')
+        with open(filename, 'w') as f:
+            json.dump(username, f)
+            print(f"we'll remember you when you come back, {username}")
+    else:
+        print(f' welcome back, {username}')
 
+
+greet_user()
